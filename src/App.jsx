98e7db17496539acb1556eb161f5a02cae951b09
krrish-exe <krishsathvara7383@gmail.com>
2026-04-01 13:36:28 +0530

    Updated React dog project with assets and components

--- src/App.jsx
+++ src/App.jsx
@@ -9,105 +9,142 @@ function App() {
 
   return (
     <>
-  <div className="background"></div> 
+      <div className="background"></div>
       <main>
-        <Canvas style={{
-          position:'fixed',
-          height:"100vh",
-          width:"100vw",
-          top:0,
-          left:0,
-          zIndex:1
+
+        <div className="images">
+
+          <img id="tomorrowland" src="tomorrowland.png" alt="" />
+          <img id="navy-pier" src="navy-pier.png" alt="" />
+          <img id="msi-chicago" src="msi-chicago.png" alt="" />
+          <img id="phone" src="phone.png" alt="" />
+          <img id="kikk" src="kikk.png" alt="" />
+          <img id="kennedy" src="kennedy.png" alt="" />
+          <img id="opera" src="opera.png" alt="" />
+        </div>
+
+
+
+        <Canvas id="canvas-elem" style={{
+          position: 'fixed',
+          height: "100vh",
+          width: "100vw",
+          top: 0,
+          left: 0,
+          zIndex: 1,
+
 
         }}>
           <Dog />
         </Canvas>
+
+        <div className="top">
+          <section id="section1">
+            <nav>
+
+              <div className="nav-elem"><svg width="200" height="40" viewBox="0 0 200 40" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                  <linearGradient id="premiumGrad" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" stopColor="#cfcfcf" />
+                    <stop offset="50%" stopColor="#ffffff" />
+                    <stop offset="100%" stopColor="#8a9eff" />
+                  </linearGradient>
+                </defs>
+
+                <text x="0" y="25"
+                  fontFamily="Inter, Helvetica, Arial, sans-serif"
+                  fontSize="20"
+                  fontWeight="500"
+                  fill="url(#premiumGrad)"
+                  letterSpacing={0.8}>
+                  KRRISH
+                </text>
+
+                <text x="95" y="25"
+                  fontFamily="Inter, Helvetica, Arial, sans-serif"
+                  fontSize="12"
+                  fontWeight="400"
+                  fill="#9a9a9a"
+                  letterSpacing={3}>
+                  STUDIO
+                </text>
+              </svg></div>
+
+
+              <div className="nav-elem"><i className="ri-play-mini-line"></i>Our Show Reel</div>
+              <div className="nav-elem second"><i className="ri-menu-fold-2-fill"></i></div>
+            </nav>
+
+
+            <div className="middle">
+              <div className="left">
+                <h1>I<br /> MAKE <br /> GOOD <br />SHIT</h1>
+              </div>
+              <div className="right"></div>
+            </div>
+            <br />
+            <div className="bottom">
+              <div className="bleft"></div>
+              <div className="bright">
+                <h2>
+                  Krrishstudio is a multidisciplinary<br />
+                  creative studio at the intersection<br />
+                  of art, design and technology.<br />
+                </h2>
+                <h4>
+                  Our goal is to deliver amazing experiences that make  <br />
+                  people talk, and build strategic value for brands, tech, <br />
+                  entertainment, arts & culture.
+                </h4>
+              </div>
+            </div>
+            <div className="fline"></div>
+            <div className="sline"></div>
+          </section>
+
+          <section id="section2">
+            <div className="titles">
+              <div img-title="tomorrowland" className="title">
+                <small>2020 - ongoing</small>
+                <h1>TomorrowLand</h1>
+              </div>
+              <div img-title="navy-pier" className="title">
+                <small>2020 - ongoing</small>
+                <h1>NavyPier</h1>
+              </div>
+
+              <div img-title="msi-chicago" className="title">
+                <small>2020 - ongoing</small>
+                <h1>MSI Chicago</h1>
+              </div>
+
+              <div img-title="phone" className="title">
+                <small>2020 - ongoing</small>
+                <h1>This was Louise's Phone</h1>
+              </div>
+
+              <div img-title="kikk" className="title">
+                <small>2020 - ongoing</small>
+                <h1>KIKK Festival 2018</h1>
+              </div>
+
+              <div img-title="kennedy" className="title">
+                <small>2020 - ongoing</small>
+                <h1>The Kennedy Center</h1>
+              </div>
+
+              <div img-title="opera" className="title">
+                <small>2020 - ongoing</small>
+                <h1>Royal Opera of Wallonia</h1>
+              </div>
+            </div>
+
+
+          </section>
+          <section id="section3"></section>
+        </div>
+
       </main>
-
-
-
-
-
-
-      <div className="top">
-      <section  id ="section1">
-        <nav>
-
-          <div className="nav-elem"><svg width="200" height="40" viewBox="0 0 200 40" xmlns="http://www.w3.org/2000/svg">
-  <defs>
-    <linearGradient id="premiumGrad" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" stopColor="#cfcfcf"/>
-      <stop offset="50%" stopColor="#ffffff"/>
-      <stop offset="100%" stopColor="#8a9eff"/>
-    </linearGradient>
-  </defs>
-
-  <text x="0" y="25"
-        fontFamily="Inter, Helvetica, Arial, sans-serif"
-        fontSize="20"
-        fontWeight="500"
-        fill="url(#premiumGrad)"
-        letterSpacing={0.8}>
-    KRRISH
-  </text>
-
-  <text x="95" y="25"
-        fontFamily="Inter, Helvetica, Arial, sans-serif"
-        fontSize="12"
-        fontWeight="400"
-        fill="#9a9a9a"
-        letterSpacing={3}>
-    STUDIO
-  </text>
-</svg></div>
-
-
-<div className="nav-elem"><i className="ri-play-mini-line"></i>Our Show Reel</div>
-<div className="nav-elem second"><i className="ri-menu-fold-2-fill"></i></div>
-        </nav>
-
-        
-        <div className="middle">
-  <div className="left">
-<h1>I<br /> MAKE <br /> GOOD <br />SHIT</h1>
-</div>
-  <div className="right"></div>
-</div>
-<br />
- <div className="bottom">
-  <div className="bleft"></div>
-  <div className="bright">
-<h2>
-Krrishstudio is a multidisciplinary<br />
-creative studio at the intersection<br />
-of art, design and technology.<br />
-</h2>
-  <h4>
-Our goal is to deliver amazing experiences that make  <br />
-people talk, and build strategic value for brands, tech, <br />
-entertainment, arts & culture.
-    </h4>
-  </div>
- </div>
-<div className="fline"></div>
-<div className="sline"></div>
-      </section>
- 
-      <section  id ="section2">
-
-
-
-
-
-
-
-
-        
-      </section>
-      <section  id ="section3"></section>
-      </div>
-
-
 
     </>
   )
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,16 +1,16 @@
 // hooks must be inside the canvas element
 
-import React, { useEffect ,useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import * as THREE from "three"
-import { Canvas,useThree } from '@react-three/fiber'
-import { OrbitControls , useGLTF ,useTexture,useAnimations} from '@react-three/drei'
+import { Canvas, useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF, useTexture, useAnimations, Texture, useProgress } from '@react-three/drei'
 // import { Environment } from "@react-three/drei"
-import { texture } from 'three/tsl'
+import { mat2, texture } from 'three/tsl'
 // import { color, sample, texture } from 'three/tsl'
 // import { DirectionalLight } from 'three'
 import { gsap } from "gsap";
 import { useGSAP } from '@gsap/react'
-import {ScrollTrigger  } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 
@@ -20,32 +20,32 @@ gsap.registerPlugin(ScrollTrigger)
 
 const Dog = () => {
 
-const model = useGLTF("/Models/dog.drc.glb")
+  const model = useGLTF("/Models/dog.drc.glb")
 
-useThree(({camera,scene,gl}) =>{
-  camera.position.z = 0.6
-  gl.toneMapping = THREE.ReinhardToneMapping
-  gl.toneMapping = THREE.ACESFilmicToneMapping
-  gl.toneMappingExposure = 1.2   // reduce brightness
-  gl.outputColorSpace = THREE.SRGBColorSpace
-},[])
+  useThree(({ camera, scene, gl }) => {
+    camera.position.z = 0.6
+    gl.toneMapping = THREE.ReinhardToneMapping
+    gl.toneMapping = THREE.ACESFilmicToneMapping
+    gl.toneMappingExposure = 1.2   // reduce brightness
+    gl.outputColorSpace = THREE.SRGBColorSpace
+  }, [])
 
-// THis hook is FOr Animation
-const {actions}=useAnimations (model.animations,model.scene)
+  // THis hook is FOr Animation
+  const { actions } = useAnimations(model.animations, model.scene)
 
-useEffect(()=>{
-  actions["Take 001"]?.play()
-},[actions])
+  useEffect(() => {
+    actions["Take 001"]?.play()
+  }, [actions])
 
-// from here we started normal mapping and using matacap material
+  // from here we started normal mapping and using matacap material
 
 
-const [normalMap, SampleMatCap,branchMap, branchNormalMap] = useTexture([
-  "/dog_normals.jpg",
-  "/matcap/mat-2.png",
-  "/branches_diffuse.jpg",
-  "/branches_normals.jpg"
-])
+  const [normalMap, SampleMatCap, branchMap, branchNormalMap] = useTexture([
+    "/dog_normals.jpg",
+    "/matcap/mat-2.png",
+    "/branches_diffuse.jpg",
+    "/branches_normals.jpg"
+  ])
 
   branchMap.flipY = false
   branchNormalMap.flipY = false
@@ -57,84 +57,294 @@ const [normalMap, SampleMatCap,branchMap, branchNormalMap] = useTexture([
 
 
 
-// here we are trying to make material once and then use it when ever we wanted
+  // / use texture matcap shit here
+const[
+  mat1,
+  mat2,
+  mat3,
+  mat4,
+  mat5,
+  mat6,
+  mat7,
+  mat8,
+  mat9,
+  mat10,
+  mat11,
+  mat12,
+  mat13,
+  mat14,
+  mat15,
+  mat16,
+  mat17,
+  mat18,
+  mat19,
+  mat20
+]= (useTexture([
+"/matcap/mat-1.png",
+"/matcap/mat-2.png",
+"/matcap/mat-3.png",
+"/matcap/mat-4.png",
+"/matcap/mat-5.png",
+"/matcap/mat-6.png",
+"/matcap/mat-7.png",
+"/matcap/mat-8.png",
+"/matcap/mat-9.png",
+"/matcap/mat-10.png",
+"/matcap/mat-11.png",
+"/matcap/mat-12.png",
+"/matcap/mat-13.png",
+"/matcap/mat-14.png",
+"/matcap/mat-15.png",
+"/matcap/mat-16.png",
+"/matcap/mat-17.png",
+"/matcap/mat-18.png",
+"/matcap/mat-19.png",
+"/matcap/mat-20.png"]).map(texture=>{
+  texture.colorSpace=THREE.SRGBColorSpace
+  return texture
+}))
 
-
-const dogMaterial =  new THREE.MeshMatcapMaterial({     
-    normalMap:normalMap,
-    matcap:SampleMatCap,  
-    transparent:false,
-    })
-
-const branchMAterial = new THREE.MeshMatcapMaterial({
-  normalMap :branchNormalMap,
-  map:branchMap
+const material = useRef({
+  uMatcap1:{value:mat2},
+  uMatcap2:{value:mat2},
+  uProgress:{value:1.0}
 })
 
 
 
 
-useEffect(()=>{
-model.scene.traverse((child) =>{
-  if (child.name.includes("DOG")){
-    child.material = dogMaterial
-  }else{
-    child.material= branchMAterial
-  }
-})
-},[model,normalMap,SampleMatCap])
+  // here we are trying to make material once and then use it when ever we wanted
 
-const refmodel = useRef()            // here we are using the refference of the actual dog model which we loaded using gltf 
-                                          // hence that dog model is saved inside that ref model
-
-
-
-
-
-useGSAP(()=>{
-
-const tl =gsap.timeline({
-  scrollTrigger:{
-    trigger:"#section1",
-    endTrigger:"#section3",
-    start:"top top",                             // it is used to tell from where animation will be started
-    end:"bottom bottom",                        // it is used to tell till where animation will be continue
-    markers:true,
-    scrub:true
-  }
+  const dogMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    matcap: mat2,
+    transparent: false,
   })
-      tl
-            .to(refmodel.current.position, {
-                z: "-=0.75",
-                y: "+=0.1"
+
+  const branchMAterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    map: branchMap
+  })
+
+
+  function onBeforeCompile(shader){
+     shader.uniforms.uMatcapTexture1 = material.current.uMatcap1
+        shader.uniforms.uMatcapTexture2 = material.current.uMatcap2
+        shader.uniforms.uProgress = material.current.uProgress
+
+        // Store reference to shader uniforms for GSAP animation
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "void main() {",
+            `
+        uniform sampler2D uMatcapTexture1;
+        uniform sampler2D uMatcapTexture2;
+        uniform float uProgress;
+
+        void main() {
+        `
+        )
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "vec4 matcapColor = texture2D( matcap, uv );",
+            `
+          vec4 matcapColor1 = texture2D( uMatcapTexture1, uv );
+          vec4 matcapColor2 = texture2D( uMatcapTexture2, uv );
+          float transitionFactor  = 0.2;
+          
+          float progress = smoothstep(uProgress - transitionFactor,uProgress, (vViewPosition.x+vViewPosition.y)*0.5 + 0.5);
+
+          vec4 matcapColor = mix(matcapColor2, matcapColor1, progress );
+        `
+        )
+
+  }
+
+
+  dogMaterial.onBeforeCompile=onBeforeCompile
+
+  useEffect(() => {
+    model.scene.traverse((child) => {
+      if (child.name.includes("DOG")) {
+        child.material = dogMaterial
+      } else {
+        child.material = branchMAterial
+      }
+    })
+  }, [model, normalMap, SampleMatCap])
+
+  const refmodel = useRef()            // here we are using the refference of the actual dog model which we loaded using gltf 
+  // hence that dog model is saved inside that ref model
+
+
+
+
+
+  useGSAP(() => {
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#section1",
+        endTrigger: "#section3",
+        start: "top top",                             // it is used to tell from where animation will be started
+        end: "bottom bottom",                        // it is used to tell till where animation will be continue
+        markers: true,
+        scrub: true
+      }
+    })
+    tl
+      .to(refmodel.current.position, {
+        z: "-=0.75",
+        y: "+=0.1"
+      })
+      .to(refmodel.current.rotation, {
+        x: `+=${Math.PI / 15}`
+      })
+      .to(refmodel.current.rotation, {
+        y: `-=${Math.PI}`,
+
+      }, "third")
+      .to(refmodel.current.position, {
+        x: "-=0.5",
+        z: "+=0.6",
+        y: "-=0.05"
+      }, "third")
+
+  }, []);
+
+// this is for the effects on the model colorchanging
+    // useEffect(()=>{
+    //   document.querySelector(`.title[img-title="tomorrowland"]`).addEventListener("mouseenter",()=>{
+    //     gsap.to(material.current.uProgress,{
+    //       value:0.0,
+    //       duration:0.3,
+    //       onComplete:()=>{
+    //         material.current.uprogress.value= material.current.umatcap1.value 
+    //         material.current.uprogress.value =1.0
+    //       }
+          
+    //     })
+    //   })
+    // })
+ useEffect(() => {
+
+        document.querySelector(`.title[img-title="tomorrowland"]`).addEventListener("mouseenter", () => {
+            material.current.uMatcap1.value = mat19
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
             })
-            .to(refmodel.current.rotation, {
-                x: `+=${Math.PI / 15}`
+        })
+        document.querySelector(`.title[img-title="navy-pier"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat8
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
             })
-            .to(refmodel.current.rotation, {
-                y: `-=${Math.PI}`,
+        })
+        document.querySelector(`.title[img-title="msi-chicago"]`).addEventListener("mouseenter", () => {
 
-            }, "third")
-            .to(refmodel.current.position, {
-                x: "-=0.5",
-                z: "+=0.6",
-                y: "-=0.05"
-            }, "third")
+            material.current.uMatcap1.value = mat9
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="phone"]`).addEventListener("mouseenter", () => {
 
-},[]);
+            material.current.uMatcap1.value = mat12
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="kikk"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat10
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="kennedy"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat8
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.title[img-title="opera"]`).addEventListener("mouseenter", () => {
+
+            material.current.uMatcap1.value = mat13
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+        document.querySelector(`.titles`).addEventListener("mouseleave", () => {
+
+            material.current.uMatcap1.value = mat2
+            
+            gsap.to(material.current.uProgress, {
+                value: 0.0,
+                duration: 0.3,
+                onComplete: () => {
+                    material.current.uMatcap2.value = material.current.uMatcap1.value
+                    material.current.uProgress.value = 1.0
+                }
+            })
+        })
+
+    }, [])
 
 
-return (
+  return (
 
-//  this section is for the orbit controls rotation position and lighting 
+    //  this section is for the orbit controls rotation position and lighting 
 
-<> 
-  <primitive object = {model.scene} 
-    scale={1.2}
-  ref={refmodel} // <--- Add this! This links the 3D object to your ref
-  position={[0.2,-0.69,0]} 
-  rotation={[0,Math.PI/+3.5,0]} />
-</>
+    <>
+      <primitive object={model.scene}
+        scale={1.2}
+        ref={refmodel} // <--- Add this! This links the 3D object to your ref
+        position={[0.2, -0.69, 0]}
+        rotation={[0, Math.PI / +3.5, 0]} />
+    </>
   )
 }
 
@@ -144,17 +354,17 @@ export default Dog
 
 
 // how can we load normal map first we have to load image in this jsx
-// for that we will make constant use texture give image path and then 
+// for that we will make constant use texture give image path and then
 // just like how we crated material for dog once and used it many times
 //we will create material for branches
-// we will make meshcap material for the map and branch map then 
+// we will make meshcap material for the map and branch map then
 // we will do traverse part where we will put else condition because we
-// only have two things one is dog and second is  branch 
+// only have two things one is dog and second is  branch
 
 
-// vertical y axis 
-//horizontal is x axis 
-// and for the depth we uses z axis 
+// vertical y axis
+//horizontal is x axis
+// and for the depth we uses z axis
 
 
 // 1. Markers
